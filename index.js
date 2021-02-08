337fb92b8d0b022e8c3806b0085cdc10a14b8b22
PRAVESH PANDEY <58443282+pravesh-pandey@users.noreply.github.com>
2021-02-08 19:25:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 var n = document.querySelectorAll(".drum").length;
 for (var i = 0; i < n; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
 
         var buttonInnerHTML = this.innerHTML;
         switch (buttonInnerHTML) {
             case "w":
-                var w1 = new Audio("sounds/tom-1.mp3");
+                var w1 = new Audio("sounds/1.mp3");
                 w1.play();
-
                 break;
             case "a":
-                var w2 = new Audio("sounds/tom-2.mp3");
+                var w2 = new Audio("sounds/2.mp3");
                 w2.play();
                 break;
             case 's':
-                var w3 = new Audio("sounds/tom-3.mp3");
+                var w3 = new Audio("sounds\3.mp3");
                 w3.play();
                 break;
             case 'd':
-                var w4 = new Audio("sounds/tom-4.mp3");
+                var w4 = new Audio("sounds/4.mp3");
                 w4.play();
                 break;
             case 'j':
-                var w5 = new Audio("sounds/kick-bass.mp3");
+                var w5 = new Audio("sounds/5.mp3");
                 w5.play();
                 break;
             case 'k':
-                var w6 = new Audio("sounds/snare.mp3");
+                var w6 = new Audio("sounds/6.mp3");
                 w6.play();
                 break;
             case 'l':
-                var w7 = new Audio("sounds/crash.mp3");
+                var w7 = new Audio("sounds/2.mp3");
                 w7.play();
                 break;
             default:
@@ -40,38 +39,38 @@ for (var i = 0; i < n; i++) {
 
     });
 }
-document.addEventListener("keypress", function(event) {
-    
-    switch (event.key) {
-            case "w":
-                var w1 = new Audio("sounds/tom-1.mp3");
-                w1.play();
+document.addEventListener("keypress", function (event) {
 
-                break;
-            case "a":
-                var w2 = new Audio("sounds/tom-2.mp3");
-                w2.play();
-                break;
-            case 's':
-                var w3 = new Audio("sounds/tom-3.mp3");
-                w3.play();
-                break;
-            case 'd':
-                var w4 = new Audio("sounds/tom-4.mp3");
-                w4.play();
-                break;
-            case 'j':
-                var w5 = new Audio("sounds/kick-bass.mp3");
-                w5.play();
-                break;
-            case 'k':
-                var w6 = new Audio("sounds/snare.mp3");
-                w6.play();
-                break;
-            case 'l':
-                var w7 = new Audio("sounds/crash.mp3");
-                w7.play();
-                break;
-            default:
-        }
+    switch (event.key) {
+        case "w":
+            var w1 = new Audio("sounds/1.mp3");
+            w1.play();
+
+            break;
+        case "a":
+            var w2 = new Audio("sounds/2.mp3");
+            w2.play();
+            break;
+        case 's':
+            var w3 = new Audio("sounds/3.mp3");
+            w3.play();
+            break;
+        case 'd':
+            var w4 = new Audio("sounds/4.mp3");
+            w4.play();
+            break;
+        case 'j':
+            var w5 = new Audio("sounds/5.mp3");
+            w5.play();
+            break;
+        case 'k':
+            var w6 = new Audio("sounds/6.mp3");
+            w6.play();
+            break;
+        case 'l':
+            var w7 = new Audio("sounds/2.mp3");
+            w7.play();
+            break;
+        default:
+    }
 });
